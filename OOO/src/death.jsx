4880141630bda6790dcca05a3a90ce3df963c2aa
emppu-07 <eemeli.sarttila@edu.tampere.fi>
2026-02-11 10:08:07 +0200

    update site, make github pages site

--- OOO/src/death.jsx
+++ OOO/src/death.jsx
@@ -4,8 +4,8 @@ function Death() {
   return (
     <div className="min-h-screen bg-black text-white flex items-center justify-center">
       <div className="text-2xl text-center">
-        <p>
-          I don't remember much about the night. But that <i>face</i> is engraved in my mind. I can hear the chanting like it's coming from this room. "three. three. three." It happened exactly 3 years, 3 months, 3 days and 3 hours ago. Bells toll around this time, yet it's so quiet. I only hear the wind howl. What is the meaning of this? That night. Has it only happened to me or could there be more victims to this... RITUAL? I'm writing this to preserve a piece of myself that once was, since from this point on It's not me who will traverse with this vessel. Truth be told, I'm too afraid to do anything myself.  
+        <p className='font-mono'>
+          I don't remember much about the night. But that <i>face</i> is engraved in my mind. I can hear the chanting like it's coming from this room. <br /> "three. three. three." <br />... It happened exactly 3 years, 3 months, 3 days and 3 hours ago. Bells toll around this time, yet it's so quiet I can hear the wind howl. <br /> What is the meaning of this? That night. Has it only happened to me or could there be more victims to this... RITUAL? I'm writing this to preserve a piece of myself that once was, since from this point on It's not me who will traverse with this vessel. Truth be told, I'm too afraid to do anything myself.  
         </p>
         <br />
         {//<p className='otherFont'>"I'll lend you my <Link to='/'>strength.</Link>"</p>
